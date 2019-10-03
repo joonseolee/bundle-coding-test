13cadd546c322dc60afab2968c3c4f31a0e31d7c
Joon-S <niceweather94@gmail.com>
2019-10-03 16:45:06 +0900

    Mod Selection Sorting way

--- algorithmus_analysis/basic/sort/Selection.py
+++ algorithmus_analysis/basic/sort/Selection.py
@@ -1,15 +1,13 @@
-def sel_sort(a):
-    max_idx = len(a)
-    for i in range(max_idx-1):
-        min_idx = i
-        for j in range(i + 1, max_idx):
-            if a[j] < a[min_idx]:
-                min_idx = j
-        a[i], a[min_idx] = a[min_idx], a[i]
-        print(a)  # 정렬 과정 출력하기
+def selection_sort(x):
+    for size in reversed(range(len(x))):
+        max_i = 0
+        for i in range(1, 1+size):
+            if x[i] > x[max_i]:
+                max_i = i
+        x[max_i], x[size] = x[size], x[max_i]
 
 
 if __name__ == '__main__':
     d = [2, 4, 5, 1, 3]
-    sel_sort(d)
+    selection_sort(d)
     print(d)

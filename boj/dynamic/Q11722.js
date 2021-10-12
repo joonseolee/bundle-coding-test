d3969354c199ce99b1a611b5302c7d8b901dfa6f
joonseolee <niceweather94@gmail.com>
2021-10-13 07:42:30 +0900

    feat(dp): 가장 긴 감소하는 부분 수열 문제 풀이

---
+++ boj/dynamic/Q11722.js
@@ -0,0 +1,40 @@
+// Run by Node.js
+// 가장 긴 감소하는 부분 수열
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+let input = [];
+
+rl.on("line", function(line) {
+    input.push(line);
+}).on("close", function() {
+    const numberList = input[1].split(' ').map(v => Number(v));
+    const dp = [];
+
+    numberList.forEach((element, index) => {
+        if (element < dp[dp.length - 1]) {
+            dp.push(element);
+            dpIndexList.push(dp.length - 1);
+        } else {
+            let left = 0;
+            let right = dp.length - 1;
+            while (left < right) {
+                let mid = parseInt((left + right) / 2);
+
+                if (dp[mid] === element) {
+                    left = mid;
+                    break;
+                } else if (dp[mid] < element) right = mid;
+                else left = mid + 1;
+            }
+            dp[left] = element;
+        }
+    });
+
+    console.log(dp.length);
+	process.exit();
+});
